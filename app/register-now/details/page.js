c5cc5355ec8648f0a1bee5e08d10ebe32b33d8d2
Sujal-88 <sujalkothale88@gmail.com>
2025-10-28 10:02:22 +0530

    membership Page Changes

--- app/register-now/details/page.js
+++ app/register-now/details/page.js
@@ -656,7 +656,7 @@ function DelegateRegistrationForm({ registrationType }) {
                             <li key={index} className="flex items-start">
                               <svg className={`flex-shrink-0 w-5 h-5 ${
                                 feature === implantFeature && addOns.implant ? 'text-green-500' :
-                                feature === banquetFeature && addOns.banquet ? 'text-red-500' :
+                                feature === banquetFeature && addOns.banquet ? 'text-green-500' :
                                 'text-purple-500' // Default purple check
                               } mr-2 mt-0.5`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
