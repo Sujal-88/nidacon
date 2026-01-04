eab005eac739c4655722c6df063a7ba8f890bfe8
Sujal Kothale <sujalkothale88@gmail.com>
2026-01-04 19:03:30 +0530

    Change grid layout from 3 to 4 columns

--- components/ExecutiveBody.js
+++ components/ExecutiveBody.js
@@ -66,7 +66,7 @@ export default function ExecutiveBody() {
         </div>
 
         {/* Members Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-12">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-12">
           {executiveBodyMembers.map((member) => (
             <div 
               key={member.id} 
